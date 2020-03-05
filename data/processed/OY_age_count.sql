660b77bafb44d2504bf29be7e06ef8a9952f3a1e
Vittorio Scacchetti <Vittorio@Vittorios-MacBook-Pro.local>
2020-03-05 09:19:38 +0000

    added queries

---
+++ data/processed/OY_age_count.sql
@@ -0,0 +1,5 @@
+SELECT agep, COUNT(agep)
+FROM pums_2017
+WHERE puma IN ('11610','11611','11612','11613','11614','11615')
+AND (agep <= 24 AND agep >= 16)
+group by agep
---
+++ data/processed/kc_coordinates.sql
@@ -0,0 +1,1 @@
+select blklatdd, blklondd, ctyname from wa_geo_xwalk where zcta = '98056'
